2a00aa3be96551945f20213f029408a1badbfcf3
ranjitjith <ranjit.jith@gmail.com>
2020-12-11 21:58:00 +0530

    End of section 7

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './members/lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
@@ -21,6 +24,9 @@ const routes: Routes = [
       {path:'messages', component:MessagesComponent}
     ]
   }, 
+  {path: 'errors', component: TestErrorsComponent},
+  {path: 'not-found', component: NotFoundComponent, pathMatch: 'full'},
+  {path: 'server-error', component: ServerErrorComponent, pathMatch: 'full'},
   {path:'**', component:HomeComponent, pathMatch: 'full'}
 ];
 
